ddc8c4be207895d3cc71b473650f295419c63f95
vikramgt <vikram.19cs116@sode-edu.in>
2023-09-23 12:21:24 +0530

    Django .py file changes

--- mind/myproject/myapp/views.py
+++ mind/myproject/myapp/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from .models import user_id
+from django.contrib.auth import login, logout
+from django.contrib import messages
+from .forms import CRUDFORM
+from django.contrib.auth.backends import BaseBackend
+from django.contrib.auth import get_user_model
+# from myapp.backends import EmailBackend
+from .authentication import EmailBackend
+from django.contrib.auth import logout
+
+def HomePage(request):
+    return render(request, 'home.html')
+
+def SignIn(request):
+    print(request.method)
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        print(email, password)
+        user = EmailBackend().authenticate(request=request, email=email, password=password, model=user_id, backend='myapp.backends.EmailBackend' )
+        print("user is ",user)
+        if user:
+            request.session['user'] = str(user).upper()
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid credentials. Please try again.')
+            return render(request, 'signin.html')
+    return render(request, 'signin.html')
+
+
+def TakeTest(request):
+    return render(request, 'taketest.html')
+
+def SignUp (request):
+    form = CRUDFORM(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect ('taketest')
+    context = {
+        "form":form
+    }
+    return render(request, 'signup.html', context)
 
 # Create your views here.
+def Logout(request):
+    logout(request)  # Log the user out
+    request.session.flush()  # Clear the session data
+    return redirect('home')  # Redirect to the home page or another desired page
--- mind/myproject/myproject/urls.py
+++ mind/myproject/myproject/urls.py
@@ -1,7 +1,7 @@
 """myproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from myapp import views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.HomePage,name='home'),
+    path('signin/',views.SignIn,name='signin'),
+    path('taketest/',views.TakeTest,name='taketest'),
+    path('signup/',views.SignUp,name='signup'),
+    path('logout/', views.Logout, name='logout'),
 ]
+
+
+
+# static files (images, css, javascript,
