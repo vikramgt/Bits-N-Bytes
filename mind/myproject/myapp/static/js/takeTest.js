0b6bd0f241f244bd033157d36be1f1810a9caf6d
vikramgt <vikram.19cs116@sode-edu.in>
2023-09-26 11:45:12 +0530

    Final Change

--- mind/myproject/myapp/static/js/takeTest.js
+++ mind/myproject/myapp/static/js/takeTest.js
@@ -20,109 +20,109 @@ const quizData = [
     },
     {
       question: "What is your gender?",
-      choice: ["Male", "Female", "Other"],
+      choice: ["Female", "Male", "Others"],
     },
     {
       question: "Are you self-employed?",
-      choice: ["yes", "No"],
+      choice: ["No", "Yes"],
     },
     {
       question: "Do you have a family history of mental illness?",
-      choice: ["yes", "No"],
+      choice: ["No", "Yes"],
     },
     {
       question:
         " If you have a mental health condition, do you feel that it interferes with your work?",
-      choice: ["often", "Sometimes", "Rarely", "Never", "Don't know"],
+      choice: ["Don't Know", "Never", "Often", "Rarely", "Sometimes"],
     },
     {
       question: "How many employees does your company or organization have?",
-      choice: ["1-5", "6-25", "26-100", "100-500", "500-1000", "1000<"],
+      choice: ["1-5", "100-500", "26-100", "500-1000","6-25", "1000>"],
     },
     {
       question:
         "Do you work remotely (outside of an office) at least 50% of the time?",
-      choice: ["yes", "No"],
+      choice: ["No", "Yes"],
     },
     {
       question: "Is your employer primarily a tech company/organization?",
-      choice: ["yes", "No"],
+      choice: ["No", "Yes"],
     },
     {
       question: "Does your employer provide mental health benefits?",
-      choice: ["yes", "No", "Don't Know"],
+      choice: ["Don't Know", "No", "Yes"],
     },
     {
       question:
         "Do you know the options for mental health care your employer provides?",
-      choice: ["yes", "No", "Not sure"],
+      choice: ["No", "Not Sure", "Yes"],
     },
     {
       question:
         "Has your employer ever discussed mental health as part of an employee wellness program?",
-      choice: ["yes", "No", "Don't Know"],
+      choice: ["Don't Know", "No", "Yes"],
     },
     {
       question:
         "Does your employer provide resources to learn more about mental health issues and how to seek help?",
-      choice: ["yes", "No", "Don't Know"],
+      choice: ["Don't Know", "No", "Yes"],
     },
     {
       question:
         "Is your anonymity protected if you choose to take advantage of mental health or substance abuse treatment resources?",
-      choice: ["yes", "No", "Don't Know"],
+      choice: ["Don't Know", "No", "Yes"],
     },
     {
       question:
         "How easy is it for you to take medical leave for a mental health condition?",
       choice: [
-        "Very easy",
-        "Some what easy",
-        "Some what difficult",
+        "Don't Know",
+        "Somewhat Difficult",
+        "Somewhat Easy",
         "Very Difficult",
-        "Dont Know",
+        "Very Easy",
       ],
     },
     {
       question:
         "Do you think that discussing a mental health issue with your employer would have negative consequences?",
-      choice: ["Yes", "No", "Maybe"],
+      choice: ["Maybe", "No", "Yes"],
     },
     {
       question:
         ":Do you think that discussing a physical health issue with your employer would have negative consequences?",
-      choice: ["Yes", "No", "Maybe"],
+      choice: ["Maybe", "No", "Yes"],
     },
     {
       question:
         "Would you be willing to discuss a mental health issue with your coworkers??",
-      choice: ["Yes", "No", "Some of them"],
+      choice: ["No", "Some of them", "Yes"],
     },
     {
       question:
         "Would you be willing to discuss a mental health issue with your direct supervisor(s)?",
-      choice: ["Yes", "No", "Some of them"],
+      choice: ["No", "Some of them", "Yes"],
     },
     {
       question:
         "Would you bring up a mental health issue with a potential employer in an interview?",
-      choice: ["Yes", "No", "Maybe"],
+      choice: ["Maybe", "No", "Yes"],
     },
     {
       question:
         "Would you bring up a physical health issue with a potential employer in an interview?",
-      choice: ["Yes", "No", "Maybe"],
+      choice: ["Maybe", "No", "Yes"],
     },
   
     {
       question:
         "Do you feel that your employer takes mental health as seriously as physical health?",
-      choice: ["Yes", "No", "Don't Know"],
+      choice: ["Don't Know", "No", "Yes"],
     },
     {
       question:
         "Have you heard of or observed negative consequences for coworkers with mental health conditions in your workplace?",
-      choice: ["Yes", "No"],
+      choice: ["No", "Yes"],
     },
   ];
   
@@ -216,9 +216,9 @@ const quizData = [
             'my_array': answerArry.map(value => typeof value === 'number' ? value : parseInt(value))
           },
           success: function(response) {
-            console.log(response);
-            alert("Success!");
-            window.location.href("{% url 'home' %}")
+            const predictions = response.predictions;
+            alert("Our ML Model has reviewed your submission. "+ predictions);
+            window.location.assign("/thank_you")
           },
           error: function(xhr, status, error) {
             console.log(error);
